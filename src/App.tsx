0745832cad9537a2c39d4953507137dfd34b656e
Douglas Falcao <falcao.douglas97@gmail.com>
2022-03-30 22:31:19 -0300

    feat: base

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import React from 'react';
-import { Routes } from 'react-router-dom';
-import Rotas from './routes';
-
-// import { Container } from './styles';
+import React from "react";
+import Rotas from "./routes";
 
 const App: React.FC = () => <Rotas />;
 
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,55 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Title } from "./styles";
-import { Calendar } from "primereact/calendar";
 import api from "../../services/api";
 import ListOfLaunch from "../../components/ListOfLaunch/ListOfLaunch";
 import { Launch } from "../../components/ListOfLaunch/ListOfLaunch";
 const Dashboard: React.FC = () => {
-  // const [date, setDate] = useState<Date | Date[] | undefined>(new Date());
   const [date15, setDate15] = useState(new Date());
   const [launch, setLaunch] = useState<Launch[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  //   {
-  //     name_launch: "Lançamento 1",
-  //     rocket: {
-  //       rocket_id: "1",
-  //       rocket_name: "Foguete 1",
-  //       rocket_type: "Normal",
-  //     },
-  //   },
-  //   {
-  //     name_launch: "Lançamento 2",
-  //     rocket: {
-  //       rocket_id: "2",
-  //       rocket_name: "Foguete 2",
-  //       rocket_type: "Normal",
-  //     },
-  //   },
-  //   {
-  //     name_launch: "Lançamento 3",
-  //     rocket: {
-  //       rocket_id: "3",
-  //       rocket_name: "Foguete 3",
-  //       rocket_type: "Normal",
-  //     },
-  //   },
-  //   {
-  //     name_launch: "Lançamento 4",
-  //     rocket: {
-  //       rocket_id: "4",
-  //       rocket_name: "Foguete 4",
-  //       rocket_type: "Normal",
-  //     },
-  //   },
-  //   {
-  //     name_launch: "Lançamento 5",
-  //     rocket: {
-  //       rocket_id: "5",
-  //       rocket_name: "Foguete 5",
-  //       rocket_type: "Normal",
-  //     },
-  //   },
-  // ]);
 
   useEffect(() => {
     api
@@ -63,6 +20,7 @@ const Dashboard: React.FC = () => {
         setIsLoading(true);
       });
   }, []);
+
   return (
     <>
       <Title>SpaceX | Falcons</Title>
